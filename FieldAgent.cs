a6658deb10b7d4e5d6c730286a1b87838eaac6f7
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 15:45:39 -0500

    Create FieldAgent.cs

---
+++ FieldAgent.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace EscapeRoomAdventure {
+   
+    public class FieldAgent {
+
+
+            public FieldAgent(string name, int code){
+              Name = name;
+              SecretCode = code;
+            }
+            public string Name
+              {get; set;}
+
+             public int SecretCode
+             {get; set;} 
+
+
+    }
+    }
