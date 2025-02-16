10a01ab4244fe97037c9a8c46af8380db196d38b
adammosaad03 <mosaadadam647@gmail.com>
2025-02-16 15:45:55 -0500

    Create File.cs

---
+++ File.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace EscapeRoomAdventure {
+
+          public class File{
+
+              public File(string fName, int fSize){
+                FileName = fName;
+                FileSize = fSize;
+              }
+
+            public string FileName
+                {get; set;}
+
+            public int FileSize
+                {get; set;}      
+
+
+          }
+
+
+}
